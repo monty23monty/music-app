ede7ecbb0693059a43272520b90044304059e532
monty23monty <74265105+monty23monty@users.noreply.github.com>
2023-07-10 23:54:08 +0100

    Sockets Working
    Display Users connected to room in a list over web sockets

--- app.py
+++ app.py
@@ -4,13 +4,20 @@ from hashlib import sha256
 from flask_sqlalchemy import SQLAlchemy
 import random
 import string
-from flask_socketio import SocketIO, emit
+import eventlet
+import eventlet.wsgi
+from flask_socketio import SocketIO, emit, join_room, leave_room, send
+from flask_migrate import Migrate
+
+
+
 
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 app.config['SECRET_KEY'] = 'vT1$n&8iD4gY7oR2cVbN9LmK3jX6zQ5wM0hA8sE4fB7pZ2xW6uJ1'
 db.init_app(app)
+socketio = SocketIO(app)
 
 
 # Import the association table
@@ -27,7 +34,8 @@ class Room(db.Model):
     creator = db.relationship('User', backref=db.backref('created_rooms', lazy=True))
     connected_users = db.relationship('User', secondary=users_rooms, backref=db.backref('rooms', lazy=True))
 
-
+    def get_connected_users(self):
+        return [user.username for user in self.connected_users]
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -45,7 +53,7 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-
+migrate = Migrate(app, db)
 
 with app.app_context():
     db.create_all()
@@ -68,8 +76,8 @@ def home():
         return redirect(url_for('login'))
 
     created_rooms = user.created_rooms if user.created_rooms else []
-    return render_template('home.html', username=user.username, created_rooms=created_rooms)
-
+    rooms = Room.query.all()
+    return render_template('home.html', username=user.username, created_rooms=created_rooms, rooms=rooms)
 
 
 @app.route("/users/create", methods=["GET", "POST"])
@@ -101,14 +109,17 @@ def user_create():
 
     return render_template("user/create.html")
 
+
 @app.route("/user/<int:id>")
 def user_detail(id):
     user = User.query.get(id)
     return render_template("user/detail.html", user=user)
 
+
 @app.route("/users")
 def auth():
     return render_template("signUp.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -156,6 +167,43 @@ def generate_random_code(length=6):
     return code
 
 
+@app.route("/room/<int:id>")
+def room_detail(id):
+    room = Room.query.get(id)
+    if not room:
+        flash('Room not found.')
+        return redirect(url_for("home"))
+
+    return render_template("room/detail.html", room=room)
+
+@socketio.on('join_room')
+def handle_join_room(data):
+    print('Received join_room event with data:', data)
+    room_id = data['room_id']
+    user_id = session['user_id']
+
+    print(f'Received join_room event. room_id: {room_id}, user_id: {user_id}')  # Added for debugging
+
+    room = Room.query.get(room_id)
+    user = User.query.get(user_id)
+
+    if room and user:
+        if user not in room.connected_users:
+            room.connected_users.append(user)
+            db.session.commit()
+
+        connected_users = room.get_connected_users()
+        print(f'Emitting connected_users_update event. users: {connected_users}')  # Added for debugging
+        # Make the user join the room in the SocketIO sense.
+        join_room(room_id)
+
+        # Emit the update only to users in this room.
+        socketio.emit('connected_users_update', {'users': room.get_connected_users()}, room=room_id)
+
+def ack():
+    print('message was received!')
+
+
 @app.route("/rooms/create", methods=["GET", "POST"])
 def room_create():
     if request.method == "POST":
@@ -172,58 +220,81 @@ def room_create():
         user.code = code
         db.session.commit()
 
+        # Emit an event to all connected clients to update the room list
+        socketio.emit('room_created', {'room_id': new_room.id, 'room_name': new_room.name})
+
+
         flash('Room created successfully!')
         return redirect(url_for("room_detail", id=new_room.id))
 
     return render_template("room/create.html")
 
+@app.route("/join", methods=["POST"])
+def join_room_route():
+    # Get room_id from the form data
+    room_id = request.form.get('room_id')
 
-@app.route("/room/<int:id>")
-def room_detail(id):
-    room = Room.query.get(id)
-    
-    # Check if the user's code matches the room's code
-    user = User.query.get(session['user_id'])
-    if room.code != user.code:
-        flash('Invalid room code!')
-        return redirect(url_for("home"))
+    # Get user_id from the session
+    user_id = session.get('user_id')
 
-    # Add the user to the connected users of the room
-    room.connected_users.append(user)
-    db.session.commit()
+    if not room_id or not user_id:
+        flash('An error occurred. Please try again.')
+        return redirect(url_for('home'))
 
-    return render_template("room/detail.html", room=room)
-
-
-@app.route('/room/<int:room_id>/join', methods=['POST'])
-def join_room(room_id):
-    room = Room.query.get(room_id)
-    user = User.query.get(session['user_id'])
-
-    if room and user:
-        room.connected_users.append(user)
-        db.session.commit()
-        flash('You have joined the room!', 'success')
-    else:
-        flash('Failed to join the room.', 'error')
-
-    return redirect(url_for('room_detail', id=room_id))
-
-@socketio.on('join_room')
-def handle_join_room(data):
-    room_id = data['room_id']
-    user_id = data['user_id']
-
+    # Get the room and the user from the database
     room = Room.query.get(room_id)
     user = User.query.get(user_id)
 
-    if room and user:
+    if not room or not user:
+        flash('An error occurred. Please try again.')
+        return redirect(url_for('home'))
+
+    # Add the user to the room's connected_users
+    if user not in room.connected_users:
         room.connected_users.append(user)
         db.session.commit()
 
-        # Emit the updated list of connected users to all clients in the room
-        emit('connected_users_update', {'users': room.get_connected_users()}, room=room_id)
+        # Emit a connected_users_update event to update the user list in real time
+        socketio.emit('connected_users_update', {'users': room.get_connected_users()}, room=room_id)
+
+    # Redirect the user to the room detail view
+    return redirect(url_for('room_detail', id=room_id))
+
+
+@app.route("/leave", methods=["POST"])
+def leave_room():
+    # Get room_id from the form data
+    room_id = request.form.get('room_id')
+
+    # Get user_id from the session
+    user_id = session.get('user_id')
+
+    if not room_id or not user_id:
+        flash('An error occurred. Please try again.')
+        return redirect(url_for('home'))
+
+    # Get the room and the user from the database
+    room = Room.query.get(room_id)
+    user = User.query.get(user_id)
+
+    if not room or not user:
+        flash('An error occurred. Please try again.')
+        return redirect(url_for('home'))
+
+    # Remove the user from the room's connected_users
+    if user in room.connected_users:
+        room.connected_users.remove(user)
+        db.session.commit()
+
+        # Emit a connected_users_update event to update the user list in real time
+        socketio.emit('connected_users_update', {'users': room.get_connected_users()}, room=room_id)
+
+    # Redirect the user to the home view
+    return redirect(url_for('home'))
+
+
 
 
 if __name__ == '__main__':
-    app.run()
+    eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 5000)), app)
+    socketio.run(app)
